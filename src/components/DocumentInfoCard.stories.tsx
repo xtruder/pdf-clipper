239cfd1943fcb93f14434e9fcb5b7f8b053b8239
Jaka Hudoklin <jaka@x-truder.net>
2022-02-10 15:12:00 +0000

    DocumentInfoCard: add a bunch of more buttons

--- src/components/DocumentInfoCard.stories.tsx
+++ src/components/DocumentInfoCard.stories.tsx
@@ -13,23 +13,47 @@ Hendrerit parturient habitant pharetra rutrum gravida porttitor eros feugiat. Mo
 sodales taciti duis praesent id. Consequat urna vitae morbi nunc congue.`;
 
 export const TheDocumentInfoCard: Story = (args) => {
-  const document: DocumentInfo = {
-    id: "id",
-    type: DocumentType.PDF,
-    title: "A brief history of time",
-    description: loremIpsum,
-    cover:
-      args.showCover &&
-      "https://images-na.ssl-images-amazon.com/images/I/A1xkFZX5k-L.jpg",
-  };
-
   return (
     <div className="w-full max-w-400 p-2 ">
-      <DocumentInfoCard document={document} />
+      <DocumentInfoCard
+        title="A brief history of time"
+        description={loremIpsum}
+        pages={args.showPages && 300}
+        lastPage={args.showLastPage && 10}
+        cover={
+          args.showCover &&
+          "https://images-na.ssl-images-amazon.com/images/I/A1xkFZX5k-L.jpg"
+        }
+        onOpen={args.onOpen}
+        onInfoClicked={args.onInfoClicked}
+        onDeleteClicked={args.onDeleteClicked}
+        onCopyLinkClicked={args.onCopyLinkClicked}
+        onExportMarkdownClicked={args.onExportMarkdownClicked}
+        onDownloadDocumentClicked={args.onDownloadDocumentClicked}
+        onDownloadAllClicked={args.onDownloadAllClicked}
+      />
     </div>
   );
 };
 
 TheDocumentInfoCard.args = {
   showCover: true,
+  showPages: true,
+  showLastPage: true,
+};
+
+TheDocumentInfoCard.argTypes = {
+  onOpen: {
+    action: "open clicked",
+  },
+  onInfoClicked: {
+    action: "info clicked",
+  },
+  onDeleteClicked: {
+    action: "delete clicked",
+  },
+  onCopyLinkClicked: { action: "copy link clicked" },
+  onExportMarkdownClicked: { action: "export markdown clicked" },
+  onDownloadDocumentClicked: { action: "download document clicked" },
+  onDownloadAllClicked: { action: "download all clicked" },
 };
--- src/components/DocumentInfoCard.tsx
+++ src/components/DocumentInfoCard.tsx
@@ -11,94 +11,139 @@ import { ReactComponent as InformationCircleIcon } from "~/assets/icons/informat
 import { EditableText } from "./EditableText";
 
 export interface DocumentInfoCardProps {
-  document: DocumentInfo;
+  title: string;
+  description?: string;
+  cover?: string;
+  lastPage?: number;
+  pages?: number;
+
+  // handlers
+  onOpen?: () => void;
+  onInfoClicked?: () => void;
+  onDeleteClicked?: () => void;
+  onCopyLinkClicked?: () => void;
+  onExportMarkdownClicked?: () => void;
+  onDownloadDocumentClicked?: () => void;
+  onDownloadAllClicked?: () => void;
 }
 
 export const DocumentInfoCard: React.FC<DocumentInfoCardProps> = ({
-  document,
-}) => {
-  const description = useRef(document.description || "");
+  title,
+  description = "",
+  cover,
+  lastPage,
+  pages,
 
+  // handlers
+  onOpen = () => null,
+  onInfoClicked = () => null,
+  onDeleteClicked = () => null,
+  onCopyLinkClicked = () => null,
+  onExportMarkdownClicked = () => null,
+  onDownloadDocumentClicked = () => null,
+  onDownloadAllClicked = () => null,
+}) => {
   return (
     <div className="card card-side card-bordered border-black shadow-lg card-compact md:card-normal overflow-visible">
-      <figure className="flex-[0.25] min-w-20 sm:min-w-40 md:min-w-40 pl-2 sm:pl-0 self-center">
-        {document.cover ? (
-          <img src={document.cover} style={{ width: "100%" }} />
+      <figure
+        className="flex-[0.25] min-w-20 sm:min-w-40 md:min-w-40 pl-2 sm:pl-0 self-center cursor-pointer"
+        onClick={() => onOpen()}
+      >
+        {cover ? (
+          <img src={cover} style={{ width: "100%" }} />
         ) : (
           <BookOpenIcon className="w-full h-full" />
         )}
       </figure>
       <div className="flex flex-[2] card-body">
         <h2 className="card-title">
-          <EditableText
-            text={document.title}
-            placeholder="Write title here..."
-          />
+          <EditableText text={title} placeholder="Write title here..." />
         </h2>
         <span className="flex-1">
           <EditableText
-            text={description.current}
+            text={description}
             placeholder="Write description here..."
             clampClassName="line-clamp-4 sm:line-clamp-3 md:line-clamp-2"
           />
         </span>
         <div className="flex flex-row items-center">
-          <progress
-            className="progress w-1/4 mr-2 hidden sm:block"
-            value="30"
-            max="100"
-          />
-          <div className="badge sm:badge-lg md:badge-lg">30/100</div>
+          {pages && (
+            <>
+              {lastPage && (
+                <progress
+                  className="progress w-1/4 mr-2 hidden sm:block"
+                  value="30"
+                  max="100"
+                />
+              )}
+              <div className="badge sm:badge-lg md:badge-lg">
+                {lastPage ? (
+                  <>
+                    {lastPage}/{pages}
+                  </>
+                ) : (
+                  <>Pages: {pages}</>
+                )}
+              </div>
+            </>
+          )}
           <div className="flex-1"></div>
           <div className="flex flex-row items-center">
-            <div className="dropdown mr-1">
+            <div className="dropdown dropdown-end mr-1">
               <div tabIndex={0} className="btn btn-xs sm:btn-sm">
                 <ShareIcon className="w-4 h-4 sm:w-6 sm:h-6" />
                 <span className="hidden md:block md:ml-1">Share</span>
               </div>
               <ul
                 tabIndex={0}
-                className="mb-1 shadow menu dropdown-content bg-base-300 rounded-box w-20 md:w-52"
+                className="mb-1 shadow menu dropdown-content bg-base-300 rounded-box w-40 md:w-52"
               >
                 <li>
-                  <a>Item 1</a>
-                </li>
-                <li>
-                  <a>Item 2</a>
-                </li>
-                <li>
-                  <a>Item 3</a>
+                  <a onClick={() => onCopyLinkClicked()}>Copy link</a>
                 </li>
               </ul>
             </div>
-            <div className="dropdown mr-1">
+            <div className="dropdown dropdown-end mr-1">
               <div tabIndex={0} className="btn btn-xs sm:btn-sm">
                 <CloudDownloadIcon className="w-4 h-4 sm:w-6 sm:h-6" />
                 <span className="hidden md:block md:ml-1">Export</span>
               </div>
               <ul
                 tabIndex={0}
-                className="shadow menu dropdown-content bg-base-300 rounded-box w-20 md:w-52"
+                className="shadow menu dropdown-content bg-base-300 rounded-box w-40 md:w-52"
               >
                 <li>
-                  <a>Item 1</a>
+                  <a onClick={() => onExportMarkdownClicked()}>
+                    Export markdown annotations
+                  </a>
                 </li>
                 <li>
-                  <a>Item 2</a>
+                  <a onClick={() => onDownloadDocumentClicked()}>
+                    Download PDF
+                  </a>
                 </li>
                 <li>
-                  <a>Item 3</a>
+                  <a onClick={() => onDownloadAllClicked()}>
+                    Download PDF and annotations as zip
+                  </a>
                 </li>
               </ul>
             </div>
-            <div className="btn btn-xs sm:btn-sm mb-[1px]">
+            <div
+              className="btn btn-xs sm:btn-sm mb-[1px]"
+              onClick={() => onDeleteClicked()}
+            >
               <TrashIcon className="w-4 h-4 sm:w-6 sm:h-6" />
               <span className="hidden md:block md:ml-1">Delete</span>
             </div>
           </div>
         </div>
       </div>
-      <div className="btn btn-xs md:btn-sm btn-primary absolute right-0 top-0 rounded-tl-none rounded-br-none md:rounded-full md:right-2 md:top-2">
+      <div
+        className="btn btn-xs md:btn-sm btn-primary absolute right-0 top-0
+          rounded-tl-none rounded-br-none md:rounded-full md:right-2 md:top-2"
+        onClick={() => onInfoClicked()}
+      >
         <InformationCircleIcon className="w-5 h-5" />
       </div>
     </div>
