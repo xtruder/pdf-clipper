ac7cf91bf6e6393da1ca241190998ae8f99e8585
Jaka Hudoklin <jaka@x-truder.net>
2022-03-09 15:19:19 +0000

    Scrollboard: convert to forwardRef, add onSize event

--- src/components/Scrollboard.tsx
+++ src/components/Scrollboard.tsx
@@ -1,35 +1,43 @@
-import React, { ForwardedRef, useEffect, useRef } from "react";
+import React, { ForwardedRef, forwardRef, useEffect, useRef } from "react";
 import useMergedRef from "@react-hook/merged-ref";
 
-export const Scrollboard: React.FC<{
-  innerRef?: ForwardedRef<HTMLDivElement | null>;
-  contentRef?: ForwardedRef<HTMLDivElement | null>;
+export interface ScrollboardProps {
   center?: boolean;
-}> = ({ children, innerRef, contentRef, center = true }) => {
-  const scrollRef = useRef<HTMLDivElement | null>(null);
+  contentRef?: ForwardedRef<HTMLDivElement | null>;
 
-  useEffect(() => {
-    if (!scrollRef.current) return;
+  onSize?: (width: number, height: number) => void;
+}
 
-    scrollRef.current.scrollLeft =
-      scrollRef.current.scrollWidth / 2 - scrollRef.current.clientWidth / 2;
-    scrollRef.current.scrollTop =
-      scrollRef.current.scrollHeight / 2 - scrollRef.current.clientHeight / 2;
-  }, [scrollRef]);
+export const Scrollboard: React.FC<ScrollboardProps> = forwardRef(
+  ({ children, contentRef, center = true, onSize }, ref) => {
+    const scrollRef = useRef<HTMLDivElement | null>(null);
 
-  return (
-    <div
-      ref={innerRef ? useMergedRef(innerRef, scrollRef) : scrollRef}
-      className="w-screen h-screen overflow-scroll bg-dark-100"
-    >
+    useEffect(() => {
+      if (!scrollRef.current) return;
+
+      scrollRef.current.scrollLeft =
+        scrollRef.current.scrollWidth / 2 - scrollRef.current.clientWidth / 2;
+      scrollRef.current.scrollTop =
+        scrollRef.current.scrollHeight / 2 - scrollRef.current.clientHeight / 2;
+
+      onSize &&
+        onSize(scrollRef.current.scrollWidth, scrollRef.current.scrollHeight);
+    }, [scrollRef]);
+
+    return (
       <div
-        ref={contentRef}
-        className={`
-          flex h-600 w-600 relative
-          ${center ? "justify-center items-center" : ""}`}
+        ref={useMergedRef(ref, scrollRef)}
+        className="w-screen h-screen overflow-scroll bg-dark-100"
       >
-        {children}
+        <div
+          ref={contentRef}
+          className={`
+            inline-flex h-600 w-600 relative
+            ${center ? "justify-center items-center" : ""}`}
+        >
+          {children}
+        </div>
       </div>
-    </div>
-  );
-};
+    );
+  }
+);
